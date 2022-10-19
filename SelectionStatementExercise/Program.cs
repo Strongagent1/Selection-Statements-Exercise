cf1237e2a940fc7f3a1e19d62a0a7d4be4d2c038
kyle jenne <strongagent1@gmail.com>
2022-10-19 16:59:58 -0400

    Selection statements first exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1001);
+
+            do
+            {
+                Console.WriteLine("Pick a number from 1-1000.");
+                var userInput = int.Parse(Console.ReadLine());
+
+
+
+                if (userInput == favNumber)
+                {
+                    Console.WriteLine($"You guessed it correct at {userInput}, way to go!");
+                }
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} is too high, try guessing a lower number.");
+                }
+
+                if (userInput < favNumber)
+
+                {
+                    Console.WriteLine($"{userInput} is too low, try guessing a higher number.");
+                }
+
+            }while (true);
         }
     }
 }
